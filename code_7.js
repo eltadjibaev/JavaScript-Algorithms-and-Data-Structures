96ee57959ad5a17b113999a7cf154cb4e304bfe5
elmu <eltadjibaev@gmail.com>
2019-05-19 17:47:49 +0900

    unshift

--- code_7.js
+++ code_7.js
@@ -53,6 +53,19 @@ class SinglyLinkedList {
         return current;
     }
 
+    unshift(val) {
+        var newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
     display() {
         var cur = this.head;
         while(cur) {
