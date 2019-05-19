7e272efa31ab2edd0eb4eeb922c13c9fd536fd4d
elmu <eltadjibaev@gmail.com>
2019-05-19 17:14:38 +0900

    pop

--- code_7.js
+++ code_7.js
@@ -25,6 +25,32 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+
+    pop() {
+        if(!this.head) return undefined; 
+        var current = this.head;
+        var oldTail = this.tail;
+        while(current.next !== oldTail) { current = current.next; }
+        this.tail = current;
+        current.next = null;
+        this.length--;
+        return oldTail;
+    }
+
+    display() {
+        var cur = this.head;
+        while(cur) {
+            console.log(cur.val);
+            cur = cur.next;
+        }
+    }
 }
 
 var list = new SinglyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.display();
+var test = list.pop();
+list.display();
+console.log(`test ==> ${test.val}`);
