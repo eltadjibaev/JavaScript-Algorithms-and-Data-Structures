139626ed10156fa90d42732b41417508c42288eb
elmu <eltadjibaev@gmail.com>
2019-05-25 18:32:46 +0900

    list 2

--- code_8.js
+++ code_8.js
@@ -13,4 +13,34 @@
          this.tail = null;
          this.length = 0;
      }
+
+     push(val) {
+         var newNode = new Node(val);
+         if(this.head === null) {
+             this.head = newNode; 
+             this.tail = newNode; 
+         } else {
+             this.tail.next = newNode;
+             newNode.prev = this.tail;
+             this.tail = newNode;
+         }
+         this.length++;
+         return this; 
+     }
+
+     display() {
+         let node = this.head;
+         let arr = [];
+         while(node !== null) {
+             arr.push(node.val);
+             node = node.next;
+         }
+         console.log(arr);
+     }
  }
+
+ var list = new DoublyLinkedList();
+ list.push(89);
+ list.push(8);
+ list.push(9);
+ list.display();
