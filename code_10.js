008cf90698b192004a4f74ce7185c672c8aab009
elmu <eltadjibaev@gmail.com>
2019-05-29 13:31:34 +0900

    class

---
+++ code_10.js
@@ -0,0 +1,16 @@
+// QUEUE FIFO
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next =null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+}
