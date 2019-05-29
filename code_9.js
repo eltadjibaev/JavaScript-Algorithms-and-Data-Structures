a5790664feb2e432fff353a19c2d9d2409c13e2d
elmu <eltadjibaev@gmail.com>
2019-05-29 13:10:41 +0900

    push

--- code_9.js
+++ code_9.js
@@ -12,4 +12,17 @@ class Stack {
         this.last = null;
         this.size = 0;
     }
+
+    push(val) {
+        let newNode = new Node(val);
+        if(!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.length;
+    }
 }
