3d832b27f4f3012ef903968a0ec828c3df2c39d4
elmu <eltadjibaev@gmail.com>
2020-04-21 16:44:09 +0900

    fibonacci

--- basic.js
+++ basic.js
@@ -9,9 +9,10 @@ console.log(addUpTo(10));*/
 
 /* Recursion Problems */
 // 1
-/*function power(num1, num2){
-    if(num2 === 0) return 1;
-    return num1 * power(num1, num2-1);
+
+/*function power(base, exponent){
+    if(exponent === 0) return 1;
+    return base * power(base, exponent-1);
 }
 
 console.log(power(2,0));   // power(2,0) // 1
@@ -36,12 +37,35 @@ console.log(factorial(7));  //factorial(7) // 5040*/
 
 // 3
 
-// productOfArray([1,2,3]) // 6
-// productOfArray([1,2,3,10]) // 60
-
-function productOfArray(arr) {
+/*function productOfArray(arr) {
     if(arr.length === 0) { return 1; }
     return arr[0] * productOfArray(arr.slice(1));
 }
 
-console.log(productOfArray([1,2,3,10]));
+console.log(productOfArray([1,2,3])); // productOfArray([1,2,3]) // 6
+console.log(productOfArray([1,2,3,10])); // productOfArray([1,2,3,10]) // 60*/
+
+//////////////////////////////////////////////////////////////////
+
+// 4 
+
+/*function recursiveRange(val){
+   if(val === 1) return 1;
+   return val + recursiveRange(val-1);
+}
+
+console.log(recursiveRange(6)); // recursiveRange(6) // 21
+console.log(recursiveRange(10)); // recursiveRange(10) // 55*/
+
+/////////////////////////////////////////////////////////
+
+// 5
+
+function fib(val){
+      if(val <= 2) return 1;
+      return fib(val-1) + fib(val - 2);
+}
+console.log(fib(4)); // 3
+console.log(fib(10)); // 55
+console.log(fib(28)); // 317811
+console.log(fib(35)); // 9227465
