58f264567bdb459f009aba77f7988da44e8b4806
elmu <eltadjibaev@gmail.com>
2020-04-22 15:47:34 +0900

    nestedEvenSum

--- basic.js
+++ basic.js
@@ -156,7 +156,41 @@ console.log(someRecursive([4,6,8])); // false */
 
   ////////////////////////////////////////////////////////////////////////
 
+  function nestedEvenSum (obj) {
+    let sum = 0;
+    function inner(obj){
+        for(let val in obj){
+            if(typeof obj[val] === 'number' && obj[val] % 2 === 0) sum += obj[val];
+            else if(typeof obj[val] === 'object') inner(obj[val]);
+        }
+    }
+    inner(obj);
+    return sum;
+  }
   
+  
+  var obj1 = {
+    outer: 2,
+    obj: {
+      inner: 2,
+      otherObj: {
+        superInner: 2,
+        notANumber: true,
+        alsoNotANumber: "yup"
+      }
+    }
+  }
+  
+  var obj2 = {
+    a: 2,
+    b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+    c: {c: {c: 2}, cc: 'ball', ccc: 5},
+    d: 1,
+    e: {e: {e: 2}, ee: 'car'}
+  };
+  
+  console.log(nestedEvenSum(obj1)); // 6
+  console.log(nestedEvenSum(obj2)); // 10
 
 
   
