5e8c844845e1ce716f7498f2aaa205aa892f01e6
elmu <eltadjibaev@gmail.com>
2020-04-23 20:13:07 +0900

    Selection Sort

---
+++ selectionSort.js
@@ -0,0 +1,12 @@
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length-1; i++) {
+        let min = i;
+        for (let j = i+1; j < arr.length; j++) {
+            if(arr[j] < arr[min]) min = j;
+        }
+        [arr[i], arr[min]] = [arr[min], arr[i]]; 
+    }
+}
+
+var arr = [29,10,14,30,37,14,18];
+selectionSort(arr); console.log(arr);
