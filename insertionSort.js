22cba6de105c9e69daf901aac8fe8d8d329acdf0
elmu <eltadjibaev@gmail.com>
2020-04-23 21:33:28 +0900

    insertion sort

---
+++ insertionSort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let val = arr[i];
+        let index = i - 1;
+        while(index >= 0 && val < arr[index]) {
+            arr[index+1] = arr[index];
+            index--;
+        }
+        arr[index+1] = val;  
+    }
+    return arr;
+}
+
+console.log(insertionSort([29,10,14,30,37,14,18, 3, 9, 80]));
+
--- selectionSort.js
+++ selectionSort.js
@@ -4,7 +4,7 @@ function selectionSort(arr) {
         for (let j = i+1; j < arr.length; j++) {
             if(arr[j] < arr[min]) min = j;
         }
-        [arr[i], arr[min]] = [arr[min], arr[i]]; 
+        if(i !== min) [arr[i], arr[min]] = [arr[min], arr[i]]; 
     }
 }
 
