6fef05a5970946f975a8a91908a2d216dcef7e68
elmu <eltadjibaev@gmail.com>
2020-04-27 19:13:48 +0900

    singly linked list

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -68,15 +68,62 @@ class singlyLinkedList{
         this.length++;
         return this;
     }
+    get(index) {
+        if(index < 0 || index >= this.length) return null;
+        let temp = this.head;
+        for (let i = 0; i < index; i++) temp = temp.next;
+        return temp;
+    }
+    set(val, index) { 
+        let node = this.get(index);
+        if(node === null) return false;
+        node.val = val;
+        return true;   
+    }
+    insert(val, index) {
+        if(index < 0 || index > this.length) return false;
+        if(index === this.length) return this.push(val);
+        if(index === 0) return this.unshift(val);
+        let newNode = new Node(val);
+        let preNode = this.get(index-1);
+        newNode.next = preNode.next;
+        preNode.next = newNode;
+        this.length++;
+        return true;
+    }
+    remove(index) {
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length-1) return this.pop();
+        let prev = this.get(index-1);
+        let oldNode = prev.next; 
+        prev.next = oldNode.next;
+        this.length--;
+        return oldNode.val;
+    }
+    reverse() {
+        if(this.length === 1) return false;
+        this.tail = this.head;
+        let node = this.head;
+        let next = node.next;
+        let prev = null;
+        for (let i = 0; i < this.length; i++) {
+            node.next = prev;
+            prev = node;
+            node = next;
+            if(next) next = next.next; 
+        }
+        this.head = prev;
+    }
 }
 
 var list = new singlyLinkedList();
-list.push('hello');
-list.push('bye');
-list.push('good bye');
+list.push(100);
+list.push(200);
+list.push(300);
+list.insert(400, 3);
 list.print();
-list.shift();
-list.shift();
-list.shift();
+list.reverse();
 console.log(list);
+list.print();
 
