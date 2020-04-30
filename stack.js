a41861c9ae7aa44d97466c261b15e15234b9d44e
elmu <eltadjibaev@gmail.com>
2020-04-30 16:53:17 +0900

    stack

--- stack.js
+++ stack.js
@@ -11,4 +11,32 @@ class Stack {
         this.last = null;
         this.size = 0;
     }
+
+    push(value) {
+        let newNode = new Node(value);
+        if(!this.first) this.first = this.last = newNode;
+        else {
+            let temp = this.first;
+            this.first = newNode;
+            newNode.next = temp;
+        }
+        return ++this.size;
+    }
+
+    pop() {
+        if(!this.first) return null;
+        let temp = this.first;
+        if(this.first === this.last) this.first = this.last = null;
+        else this.first = temp.next;
+        this.size--;
+        return temp.value;
+    }
 }
+
+var stack = new Stack();
+stack.push(23);
+stack.push(22);
+stack.push(21);
+console.log(stack);
+console.log(stack.pop());
+console.log(stack);
