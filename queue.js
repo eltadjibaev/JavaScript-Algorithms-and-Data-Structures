b083a158e0aa430d2ffe54f20eb6b5ab1c619b5f
elmu <eltadjibaev@gmail.com>
2020-04-30 17:28:07 +0900

    queue

---
+++ queue.js
@@ -0,0 +1,32 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(value) {
+        let newNode = new Node(value);
+        if(!this.first) this.first = this.last = newNode;
+        else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    dequeue() {
+        if(!this.first) return undefined;
+        let temp = this.first;
+        if(this.first === this.last) this.first = this.last = null;
+        else this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
