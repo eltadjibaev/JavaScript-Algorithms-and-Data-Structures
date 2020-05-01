a63eb4798c82cfc1306167b1ca7ba24227c3fbc6
elmu <eltadjibaev@gmail.com>
2020-05-01 18:46:34 +0900

    test

--- binaryHeap.js
+++ binaryHeap.js
@@ -9,6 +9,27 @@ class MaxBinaryHeap {
         this.bubbleUp();
     }
 
+    removing() {
+        [this.values[0], this.values[this.values.length-1]] = [this.values[this.values.length-1], this.values[0]];
+        this.values.pop();
+        this.sinkDown();
+    }
+
+    sinkDown() {
+        let parentIndex = 0;
+        let leftIndex = 2 * parentIndex + 1;
+        let rightIndex = 2 * parentIndex + 2;
+        if(this.values[parentIndex] < this.values[leftIndex] || this.values[parentIndex] < this.values[rightIndex]) {
+            if(this.values[leftIndex] > this.values[rightIndex]) {
+                [this.values[leftIndex], this.values[parentIndex]] = [this.values[parentIndex], this.values[leftIndex]];
+                parentIndex = leftIndex; 
+            } else {
+                [this.values[rightIndex], this.values[parentIndex]] = [this.values[parentIndex], this.values[rightIndex]]; 
+                parentIndex = rightIndex;
+            }
+        }
+    }
+
     bubbleUp() {
         let index = this.values.length - 1;
         const element = this.values[index];
