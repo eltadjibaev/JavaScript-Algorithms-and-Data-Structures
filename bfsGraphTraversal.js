11c18e1b536c22aca692cdeeec024caf23b77ee9
elmu <eltadjibaev@gmail.com>
2020-05-03 19:48:52 +0900

    BFS Graph Traversal

---
+++ bfsGraphTraversal.js
@@ -0,0 +1,89 @@
+class Graph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+
+    addEdge(v1, v2) {
+        if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
+            this.adjacencyList[v1].push(v2);
+            this.adjacencyList[v2].push(v1);
+        }
+    }
+
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
+    }
+
+    removeVertex(v) {
+        while (this.adjacencyList[v].length) {
+            const adjacentVertex = this.adjacencyList[v].pop();
+            this.removeEdge(v, adjacentVertex);
+        }
+        delete this.adjacencyList[v];
+    }
+
+    depthFirstIterative(start) {
+        const stack = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[start] = true;
+        while (stack.length) {
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    stack.push(neighbor); 
+                    visited[neighbor] = true;
+                }
+            });
+        }
+        return result;
+    }
+
+    breadthFirst(start) {
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[start] = true;
+        while (queue.length) {
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
+}
+
+let g = new Graph();
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('C', 'E');
+g.addEdge('D', 'E');
+g.addEdge('D', 'F');
+g.addEdge('E', 'F');
+
+console.log(g.breadthFirst('A'));
