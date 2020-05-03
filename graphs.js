17afbdc77f5e459aa3ef5067192407f219bca8f8
elmu <eltadjibaev@gmail.com>
2020-05-03 15:13:35 +0900

    add vertex and add edge

---
+++ graphs.js
@@ -0,0 +1,17 @@
+
+class Graph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+
+    addEdgeVertex(v1, v2) {
+        if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
+            this.adjacencyList[v1].push(v2);
+            this.adjacencyList[v2].push(v1);
+        }
+    }
+}
