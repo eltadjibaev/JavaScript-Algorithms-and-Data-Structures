f42463d32cdbd2c562cc2837557986ebb34bd817
elmu <eltadjibaev@gmail.com>
2020-05-08 18:00:38 +0900

    find

--- codingExercise-1.js
+++ codingExercise-1.js
@@ -110,71 +110,16 @@ console.log(bst.root.right.right.right.left.left.value);*/
 
 
 
-class MaxBinaryHeap {
-    constructor() {
-        this.values = [];
-    }
 
-    insert(value) {
-        this.values.push(value);
-        this.bubbleUp();
-    }
-
-    removing() {
-        [this.values[0], this.values[this.values.length-1]] = [this.values[this.values.length-1], this.values[0]];
-        let max = this.values.pop();
-        this.sinkDown();
-        return max;
-    }
-
-    sinkDown() {
-        let idx = 0;
-        while (this.values.length > (2 * idx + 1)) {
-            let leftIdx = 2 * idx + 1;
-            let rightIdx = 2 * idx + 2;
-            if(this.values[leftIdx] && this.values[idx] < this.values[leftIdx] || this.values[rightIdx] && this.values[idx] < this.values[rightIdx]) {
-                if(this.values[leftIdx] && this.values[rightIdx]) {
-                    if(this.values[leftIdx] > this.values[rightIdx]) {
-                        [this.values[leftIdx], this.values[idx]] = [this.values[idx], this.values[leftIdx]];
-                        idx = leftIdx; 
-                    } else {
-                        [this.values[rightIdx], this.values[idx]] = [this.values[idx], this.values[rightIdx]]; 
-                        idx = rightIdx;
-                    }
-                } else {
-                    if(this.values[leftIdx]) {
-                        [this.values[leftIdx], this.values[idx]] = [this.values[idx], this.values[leftIdx]];
-                        idx = leftIdx; 
-                    } else {
-                        [this.values[rightIdx], this.values[idx]] = [this.values[idx], this.values[rightIdx]]; 
-                        idx = rightIdx;
-                    }
-                } 
-            } else break;
-        }
-    }
-
-    bubbleUp() {
-        let index = this.values.length - 1;
-        const element = this.values[index];
-        while(index > 0) {
-            let parentIndex = Math.floor((index-1) / 2);
-            if(element <= this.values[parentIndex]) break;
-            [this.values[parentIndex], this.values[index]] = [this.values[index], this.values[parentIndex]];
-            index = parentIndex; 
-        }
-    }
+function name(nums) {
+  let res = [];
+  let obj = {};
+  for (let i = 0; i < nums.length; i++) {
+      if(obj[nums[i]]) obj[nums[i]]++;
+      else obj[nums[i]] = 1;
+  } 
+  console.log(obj); 
 }
 
-let MaxHeap = new MaxBinaryHeap();
-MaxHeap.insert(1);
-MaxHeap.insert(2);
-MaxHeap.insert(3);
-MaxHeap.insert(4);
-MaxHeap.insert(5);
-MaxHeap.insert(6);
-console.log(MaxHeap.values);
-console.log(MaxHeap.removing());
-console.log(MaxHeap.values);
-
-
+name([4,3,2,7,8,2,3,1]);
+//console.log(name('aabbcc', 'bcabcaddff'));
